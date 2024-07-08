f7985fc487393a4278f97774dd2aec6ab3798638
sultanbeishenkulov <57196071+sultanbeish@users.noreply.github.com>
2024-07-08 14:38:27 -0500

    made production ready code, left to solve problem with vector representation of promts

---
+++ main.py
@@ -0,0 +1,11 @@
+from langchain_setup import get_few_shot_db_chain
+import streamlit as st
+
+st.title("Rude Manners T Shirts: Database Q&A")
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_few_shot_db_chain()
+    answer = chain.run(question)
+    st.header("Answer: ")
+    st.write(answer)
